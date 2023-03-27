7f43927ec1329aa7971f996c6b0037cac6448ae3
David Padilla <davidpaag2002@gmail.com>
2023-03-27 12:12:35 +0200

    feat(user): Added update user info controllers

---
+++ src/controllers/user-update.controller.js
@@ -0,0 +1,18 @@
+import UserModel from "#Schemas/user.shema.js";
+
+const userUpdateInfoController = async (req, res) => {
+    const { id } = req;
+    const { name, surname } = req.body;
+
+    const existingUserById = await UserModel.findById(id).exec();
+    if (!existingUserById) return res.status(401).send('Usuario no autorizadoooo');
+
+    existingUserById.name = name;
+    existingUserById.surname = surname;
+
+    await existingUserById.save();
+
+    return res.send('Usuario actualizado');
+}
+
+export default userUpdateInfoController
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,14 +1,16 @@
 import { Router } from 'express'
 import userLoginDTO from '#Dto/user-login.dto.js';
 import userRegisterDTO from '#Dto/user-register.dto.js';
-// import userUpdateInfoDTO from '#Dto/user-update-info.dto.js';
-// import userUpdateEmailDTO from '#Dto/user-update-email.dto.js';
-// import userUpdatePasswordDTO from '#Dto/user-update-password.dto.js';
-// import userDeleteDTO from '#Dto/user-delete.dto.js';
 import userJWTDTO from '#Dto/user-jwt-dto.js';
 import userRegisterController from '#Controllers/user-register.controller.js';
 import userLoginController from '#Controllers/user-login.controller.js';
 import userProfileController from '#Controllers/user-profile.controller.js';
+import userUpdateInfoDTO from '#Dto/user-update-info.dto.js';
+import userUpdateInfoController from '#Controllers/user-update.controller.js';
+import userUpdateEmailDTO from '#Dto/user-update-email.dto.js';
+import userUpdateEmailController from '#Controllers/user-update-email.controller.js';
+import userUpdatePasswordDTO from '#Dto/user-update-password.dto.js';
+import userUpdatePasswordController from '#Controllers/user-update-password.controller.js';
 
 const userRouter = Router();
 
@@ -18,11 +20,11 @@ userRouter.post("/login", userLoginDTO, userLoginController);
 
 userRouter.get("/profile", userJWTDTO, userProfileController);
 
-// userRouter.patch("/update-info", userJWTDTO, userUpdateInfoDTO, userUpdateInfoController);
+userRouter.patch("/update-info", userJWTDTO, userUpdateInfoDTO, userUpdateInfoController);
 
-// userRouter.patch("/update-email", userJWTDTO, userUpdateEmailDTO, userUpdateEmailController)
+userRouter.patch("/update-email", userJWTDTO, userUpdateEmailDTO, userUpdateEmailController)
 
-// userRouter.patch("/update-password", userJWTDTO, userUpdatePasswordDTO, userUpdatePasswordController);
+userRouter.patch("/update-password", userJWTDTO, userUpdatePasswordDTO, userUpdatePasswordController);
 
 // userRouter.delete("/remove-user", userJWTDTO, userDeleteDTO, userDeleteController);
 
